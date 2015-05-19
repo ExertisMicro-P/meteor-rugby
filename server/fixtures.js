0a4fd447f47f782b29e858b6a2759ff245dcd104
Russell Hutson <russell.hutson@exertismicro-p.co.uk>
2015-05-19 15:37:41 +0100

    Templates renamed, Fixtures can pull from CSV file

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,25 +1,72 @@
-var numreqd=80;
-var curnumResellers = Resellers.find().count() ;
 
-console.log('numreqd='+numreqd);
-console.log('curnumResellers='+curnumResellers);
+Meteor.startup(function () {
+    var USECSVFILE = true;
 
+    if (USECSVFILE) {
+        // read data from a CSV file
+        // @see https://atmospherejs.com/evaisse/csv
+        //console.log('pwd='+__meteor_bootstrap__.serverDir);
 
-if (curnumResellers < numreqd) {
-  
-  var target = numreqd - curnumResellers;
-  for (var i=0; i<target; i++) {
-    var newRecord =  {
-        name: Fake.user({fields: ['fullname']}).fullname,
-        achieved: Math.floor((Math.random() * 100) + 1),
-        league: Fake.fromArray(['Dell', 'Lenovo', 'Samsung', 'Belkin','TP-Link','Netgear'])
-      };
-    Resellers.insert(newRecord);
-    
-    console.log(newRecord);
-  } // for
+        dataToLoad = [];
 
 
-}//if
+        //var fs = Npm.require('fs');
+        //var data = fs.readFileSync('../web.browser/app/data.csv', 'utf8');
 
- 
+        //CSV.readCsvFileLineByLine('../web.browser/app/data.csv', {
+
+        CSV.readCsvFileLineByLine('../web.browser/app/data.csv', {
+                        headers: true,
+                        delimiter: ","
+                    }, function (line) {
+                        dataToLoad.push(line);
+                    });
+
+        //console.log('dataToLoad ='+JSON.stringify(dataToLoad));
+        for(record in dataToLoad) {
+            var newRecord = {
+                    name: dataToLoad[record].name.trim(),
+                    achieved: parseInt(dataToLoad[record].achieved),
+                    league: dataToLoad[record].league.trim()
+                };
+            console.log(JSON.stringify(newRecord));
+            Resellers.insert(newRecord);
+        }
+
+
+
+
+
+
+    } else {
+
+        // test fixtures
+
+        var numreqd = 80;
+        var curnumResellers = Resellers.find().count();
+
+        console.log('numreqd=' + numreqd);
+        console.log('curnumResellers=' + curnumResellers);
+
+
+        if (curnumResellers < numreqd) {
+
+            var target = numreqd - curnumResellers;
+            for (var i = 0; i < target; i++) {
+                var newRecord = {
+                    name: Fake.user({fields: ['fullname']}).fullname,
+                    achieved: Math.floor((Math.random() * 100) + 1),
+                    league: Fake.fromArray(['Dell', 'Lenovo', 'Samsung', 'Belkin', 'TP-Link', 'Netgear'])
+                };
+                Resellers.insert(newRecord);
+
+                console.log(newRecord);
+            } // for
+
+
+        }//if
+
+    } // if USECSVFILE
+
+
+});

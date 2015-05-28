ab1d5c9008be7e103b0516264284e2a42f7fb49f
Russell Hutson <russell.hutson@exertismicro-p.co.uk>
2015-05-28 16:58:46 +0100

    Change to use <table> for league tables and add a rank

--- client/templates/resellers/resellers_list.js
+++ client/templates/resellers/resellers_list.js
@@ -7,7 +7,11 @@ Template.resellersList.helpers({
   resellersForThisLeague: function(leagueName) {
     leagueData = Template.parentData(0);
     console.log('leagueName='+leagueData.name);
-    filteredResellers = Resellers.find( {searchableLeague: leagueData.name.toLowerCase()}, {sort:{achieved:-1}});
+    filteredResellers = Resellers.find( {searchableLeague: leagueData.name.toLowerCase()}, {sort:{achieved:-1}}).map(function(reseller, index) {
+      reseller.rank = index+1;
+      return reseller;
+    });;
+
     return filteredResellers;
   }
 });
@@ -20,8 +24,12 @@ Template.resellersListShort.helpers({
     leagueData = Template.parentData(0);
     console.log('leagueName='+leagueData.name);
     //filteredResellers = _.where(Resellers.find({}), { league: leagueData.name});
-    filteredResellers = Resellers.find( { searchableLeague: leagueData.name.toLowerCase()}, {sort:{achieved:-1}, limit:limit});
+    filteredResellers = Resellers.find( { searchableLeague: leagueData.name.toLowerCase()}, {sort:{achieved:-1}, limit:limit}).map(function(reseller, index) {
+      reseller.rank = index+1;
+      return reseller;
+    });
     //return _.sortBy(filteredResellers, 'acheived').reverse().slice(0, limit);
+
     return filteredResellers;
   }
 });
